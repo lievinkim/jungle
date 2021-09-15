91210dd08415249aa95a05dd2735a6d9d2408a36
Lievin Kim <woogisky@naver.com>
2021-09-15 22:32:44 +0900

    2021.09.15

--- week0508_cproject/c_lecture/practice_cpp.cpp
+++ week0508_cproject/c_lecture/practice_cpp.cpp
@@ -2,34 +2,69 @@
 #include <algorithm>
 
 using namespace std;
-long long dist[100000];
-long long oil_price[100000];
+long long dp[91];
 
 int main() {
+
     int n;
-
     cin >> n;
-    for (int i=0; i<n-1; i++) {
-        cin >> dist[i];
-    }
-    for (int i=0; i<n; i++) {
-        cin >> oil_price[i];
-    }
 
-    long long price = oil_price[0];
-    long long expense = 0;
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 2;
 
-    for (int i=0; i<n-1; i++) {
-        expense += dist[i] * price;
-
-        if (oil_price[i+1] < price) {
-            price = oil_price[i+1];
+    for (int i=4; i<=90; i++) {
+        int temp = 0;
+        for (int j=0; j<=i-2; j++) {
+            temp += dp[j];
         }
+        dp[i] = temp;
     }
-    
-    cout << expense << endl;
+
+    cout << dp[n] << endl;
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+// long long dist[100000];
+// long long oil_price[100000];
+
+// int main() {
+//     int n;
+
+//     cin >> n;
+//     for (int i=0; i<n-1; i++) {
+//         cin >> dist[i];
+//     }
+//     for (int i=0; i<n; i++) {
+//         cin >> oil_price[i];
+//     }
+
+//     long long price = oil_price[0];
+//     long long expense = 0;
+
+//     for (int i=0; i<n-1; i++) {
+//         expense += dist[i] * price;
+
+//         if (oil_price[i+1] < price) {
+//             price = oil_price[i+1];
+//         }
+//     }
+    
+//     cout << expense << endl;
+//     return 0;
+// }
 
 
 
